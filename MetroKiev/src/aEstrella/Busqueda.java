0472822f7ce3372edd4a9998ce350db0ec9dde17
Andres <zamo1207@users.noreply.github.com>
2022-11-16 15:58:33 +0100

    Updated

--- MetroKiev/src/aEstrella/Busqueda.java
+++ MetroKiev/src/aEstrella/Busqueda.java
@@ -17,6 +17,7 @@ public class Busqueda {
 	public static Estacion aEstrella(Estacion origen, Estacion destino) {
 		PriorityQueue<Estacion> listaCerrada = new PriorityQueue<Estacion>();
 	    PriorityQueue<Estacion> listaAbierta = new PriorityQueue<Estacion>();
+	    boolean nuevo;
 
 	    
 	    origen.f = origen.g + origen.getH(destino);
@@ -32,7 +33,8 @@ public class Busqueda {
 	            double pesoTotal = n.g + c.distancia;
 	            
 
-	            if(!listaAbierta.contains(
+	            if(!listaAbierta.contains(estacion) && !listaCerrada.contains(estacion)){
+	                estacion.raiz = n;
 	                estacion.g = pesoTotal;
 	                estacion.f = estacion.g + estacion.getH(destino);
 	                listaAbierta.add(estacion);
@@ -65,8 +67,7 @@ public class Busqueda {
 		Estacion e = destino;
 
 	    if(e==null) return null;
-estacion) && !listaCerrada.contains(estacion)){
-	                estacion.raiz = n;
+
 	    ArrayList<Estacion> ids = new ArrayList<>();
 
 	    while(e.raiz != null){
